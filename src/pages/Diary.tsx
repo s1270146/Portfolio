6e23a6ff183b8b70e5e1f8bd6ed6556871ace27b
s1270146 <s1270146@gmail.com>
2022-12-22 18:48:18 +0900

    ブログ追加

--- src/pages/Diary.tsx
+++ src/pages/Diary.tsx
@@ -10,26 +10,45 @@ function Diary() {
     <div className={style.App}>
       <Header />
       <main>
-        <h1
-        >
-          Blog
-        </h1>
+        <h1>Blog</h1>
+        <a href="https://awake-split-999.notion.site/Flutter-FVM-4fd6f7465647427183df1d86df057625">
+          <article>
+            <img src={flutterImage} alt="" />
+            <div className={style.article_sentence}>
+              <h2>【Flutter】FVMについて</h2>
+              <p></p>
+            </div>
+          </article>
+        </a>
+        <a href="https://awake-split-999.notion.site/Flutter-FVM-4fd6f7465647427183df1d86df057625">
+          <article>
+            <img src="https://riverpod.dev/ja/img/logo.png" alt="" />
+            <div className={style.article_sentence}>
+              <h2>【Flutter】Riverpodについて part2【メモ】</h2>
+              <p></p>
+            </div>
+          </article>
+        </a>
         <a href="https://awake-split-999.notion.site/SciPy-t-89f8fc12bc3b410b8d501fd556fb6389">
-          <article
-          >
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtatXVk-WJXgxKgT8r_RsRxQmBxd6dri4yMro0dwdkNRkku7umOhqRDZR2maRBCVK9xm0&usqp=CAU" alt="" />
+          <article>
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtatXVk-WJXgxKgT8r_RsRxQmBxd6dri4yMro0dwdkNRkku7umOhqRDZR2maRBCVK9xm0&usqp=CAU"
+              alt=""
+            />
             <div className={style.article_sentence}>
               <h2>【統計】SciPyを使ってt検定を行う</h2>
               <p>
-              研究で出てきた二種類のデータの平均で比較していたところ、教授から「統計学を使ってもっと詳しく比較してみた方がよい」と言われました。そこで二つのデータを比較するためのt検定というのを見つけました。
+                研究で出てきた二種類のデータの平均で比較していたところ、教授から「統計学を使ってもっと詳しく比較してみた方がよい」と言われました。そこで二つのデータを比較するためのt検定というのを見つけました。
               </p>
             </div>
           </article>
         </a>
         <a href="https://awake-split-999.notion.site/Flutter-Riverpod-part1-f20b0f55b5964d938def00131fba220a">
-          <article
-          >
-            <img src="https://riverpod.dev/ja/img/logo.png" alt="https://riverpod.dev/ja/img/logo.png" />
+          <article>
+            <img
+              src="https://riverpod.dev/ja/img/logo.png"
+              alt="https://riverpod.dev/ja/img/logo.png"
+            />
             <div className={style.article_sentence}>
               <h2>【Flutter】Riverpodについて part1【メモ】</h2>
               <p>
@@ -40,8 +59,7 @@ function Diary() {
           </article>
         </a>
         <a href="https://awake-split-999.notion.site/Flutter-freezed-8ceb926c57d04db2af2076b91523a8bc">
-          <article
-          >
+          <article>
             <img src={flutterImage} alt="" />
             <div className={style.article_sentence}>
               <h2>【Flutter】freezedについて【メモ】</h2>
@@ -52,8 +70,7 @@ function Diary() {
           </article>
         </a>
         <a href="https://awake-split-999.notion.site/Flutter-Dart-immutable-363e87e806e844969d416b872b8d1437">
-          <article
-          >
+          <article>
             <img src={dartImage} alt="" />
             <div className={style.article_sentence}>
               <h2>【Flutter】【Dart】immutableについて【メモ】</h2>
